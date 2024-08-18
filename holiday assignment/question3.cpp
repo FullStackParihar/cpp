0c6d38db143e46c4079f7b669ccf4c08b0fd6772
vishnuparihar <vishnuparihar239925@gmail.com>
2024-08-18 19:33:39 +0530

    holiday assignment

--- holiday assignment/question3.cpp
+++ holiday assignment/question3.cpp
@@ -1,17 +1,29 @@
+ //Create a 2x2 matrix and a 2x2 matrix with user-defined values.
+ 
  #include <bits/stdc++.h>
 using namespace std;
 
 int main()
-{
-     int vishnu[]={1,2,3,4};
-
-     cout<< vishnu[3] ;
-
-     cin>> vishnu[0]  ;
-
-     cout<< vishnu[0];
-
-
+{  int arr[3][3];
+ 
+ arr[0][0]=2;
+ arr[0][1]=2;
+ arr[1][0]=4;
+ arr[1][1]=4;
+ 
+ 
+ for(int i=0; i<2; i++)
+ {
+      
+      for(int j=0; j<2; j++)
+      {
+      
+     cout<<arr[i][j]<<" ";
+      
+       }
+ 
+    cout<<endl;
+}
 
     return 0;
 }
