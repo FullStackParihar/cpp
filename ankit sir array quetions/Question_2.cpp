a07952755b8afd84f5ab0eae04d14ce1b16459b0
vishnuparihar <vishnuparihar239925@gmail.com>
2024-08-29 00:08:22 +0530

    assignment

--- ankit sir array quetions/Question_2.cpp
+++ ankit sir array quetions/Question_2.cpp
@@ -1,7 +1,6 @@
-/*2. Reverse an Array 
-o Explanation: Reverse the order of elements in the array. 
-o Given Array: {1, 2, 3, 4, 5} 
-o Expected Array: {5, 4, 3, 2, 1}  */
+/*3. Remove Duplicates from a Sorted Array 
+o Explanation: Remove duplicates and return the unique elements. o Given Array: {1, 1, 2, 3, 3, 4} 
+o Expected Array: {1, 2, 3, 4}  */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -9,22 +8,14 @@ using namespace std;
 int main()
 {
 
-    int a[] ={1, 2, 3, 4, 5};
+    int a[] ={1, 1, 2, 3, 3, 4} ;
 
-    int rev;
-    int j=4;
-    for (int i = 0; i < 2; i++)
+    
+    for (int i = 0; i < 7; i++)
     {
-          rev=a[i];
-          a[i]=a[j];
-          a[j]=rev;
-    j--;
+         
     }
     
-    for(int i=0;i<5;i++)
-    {
-        cout<<a[i];
-    }
-
+     
     return 0;
 }
