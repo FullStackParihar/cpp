d670533ad72a4ab32d4a6ee53bcf0c8eab71b3e8
vishnuparihar <vishnuparihar239925@gmail.com>
2024-08-30 09:51:42 +0530

    array Questions

---
+++ ankit sir array quetions/Question_9.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int findMostFrequentElement(int arr[], int n) {
+    int maxCount = 0;
+    int mostFrequent = arr[0];
+
+   
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+     
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        
+        if (count > maxCount) {
+            maxCount = count;
+            mostFrequent = arr[i];
+        }
+    }
+
+    return mostFrequent;
+}
+
+int main() {
+    int arr[] = {1, 3, 2, 1, 4, 1, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = findMostFrequentElement(arr, n);
+
+    std::cout << "The most frequent element is: " << result << std::endl;
+
+    return 0;
+}
