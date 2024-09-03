9d4dbe3fb2c881eedbb8cc5fd8bcc4e3d567cc00
vishnuparihar <vishnuparihar239925@gmail.com>
2024-09-03 10:10:15 +0530

    array Questions

--- ankit sir array quetions/Question_3.cpp
+++ ankit sir array quetions/Question_3.cpp
@@ -1,3 +1,7 @@
+// 3. Remove Duplicates from a Sorted Array 
+// o Explanation: Remove duplicates and return the unique elements. o Given Array: {1, 1, 2, 3, 3, 4} 
+// o Expected Array: {1, 2, 3, 4} 
+
 #include <iostream>
 using namespace std;
 
---
+++ ankit sir array quetions/Question_6.cpp
@@ -0,0 +1,5 @@
+// 6. Find the Index of the First Occurrence of a Value 
+// o Explanation: Return the index of the first appearance of a specified value. o Given Array: {10, 20, 30, 40, 50} 
+// o Search Value: 30 
+// o Expected Result: 2
+
