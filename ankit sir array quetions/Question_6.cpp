1f8e6a2c8561cf51288cff4e484478618143e5b8
vishnuparihar <vishnuparihar239925@gmail.com>
2024-09-03 10:17:56 +0530

    array Questions

--- ankit sir array quetions/Question_6.cpp
+++ ankit sir array quetions/Question_6.cpp
@@ -3,3 +3,36 @@
 // o Search Value: 30 
 // o Expected Result: 2
 
+#include <iostream>
+using namespace std;
+
+int occurindex(int arr[], int n)
+{
+    if (n == 0)
+        return 0;
+
+     
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 30)
+        {
+           
+             return i;
+        }
+    }
+
+    return 0;
+}
+
+    int main()
+    {
+        int arr[] = {10, 20, 30, 40, 50};
+        int n = sizeof(arr) / sizeof(arr[0]);
+
+        int firstoccur = occurindex(arr, n);
+
+        cout << "Expected result: "<< firstoccur;
+         
+
+        return 0;
+    }
