0ca0f09255af1e27142104b20d2c350d82c0f39d
vishnuparihar <vishnuparihar239925@gmail.com>
2024-09-15 22:40:51 +0530

    assignment

---
+++ CPP 200days challenge/Day 3/day 3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPerfectNumber(int num) {
+    if (num < 2) return false;
+    int sum = 0;
+    for (int i = 1; i <= num / 2; i++)
+        if (num % i == 0) sum += i;
+    return sum == num;
+}
+
+int main() {
+    int num;
+    cout << "Enter a positive integer: ";
+    cin >> num;
+    if (num <= 0) {
+        cout << "The number must be positive!" << endl;
+        return 0;
+    }
+    if (isPerfectNumber(num))
+        cout << num << " is a perfect number." << endl;
+    else
+        cout << num << " is not a perfect number." << endl;
+    return 0;
+}
