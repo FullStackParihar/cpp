9f06558cb1cc6dd61701d784b7c8fb610cd12221
vishnuparihar <vishnuparihar239925@gmail.com>
2024-09-17 20:49:15 +0530

    200 day challenge

--- CPP 200days challenge/Day 6/day 6.cpp
+++ CPP 200days challenge/Day 6/day 6.cpp
@@ -1,34 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void countCharacters(const string &str, int &letterCount, int &digitCount, int &otherCount) {
+void countCharacters(const string &str, int &letterCount, int &digitCount, int &otherCount)
+{
     letterCount = 0;
     digitCount = 0;
-                     otherCount = 0;
+    otherCount = 0;
 
-    for (char ch : str) {
-        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+    for (char ch : str)
+    {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        {
             letterCount++;
-        } else if (ch >= '0' && ch <= '9') {
-                                   digitCount++;
-        } else {
+        }
+        else if (ch >= '0' && ch <= '9')
+        {
+            digitCount++;
+        }
+        else
+        {
             otherCount++;
         }
     }
 }
 
-int main() {
+int main()
+{
     string input;
     int letterCount, digitCount, otherCount;
 
     cout << "Enter a string: ";
     getline(cin, input);
-              
-                               countCharacters(input, letterCount, digitCount, otherCount);
+
+    countCharacters(input, letterCount, digitCount, otherCount);
 
     cout << "Number of letters: " << letterCount << endl;
     cout << "Number of digits: " << digitCount << endl;
-                                 cout << "Number of other characters: " << otherCount << endl;
+    cout << "Number of other characters: " << otherCount << endl;
 
     return 0;
 }
