4e125a9d07f38a41ea9c6d7d06e6bbcd080f116a
vishnuparihar <vishnuparihar239925@gmail.com>
2024-09-24 09:42:41 +0530

    200 day challenge

--- CPP 200days challenge/Day 11/day 11.cpp
+++ CPP 200days challenge/Day 11/day 11.cpp
@@ -1,3 +1,6 @@
+// Sum Calculation: This function calculates the sum of integers from 1 to n using a simple loop.
+// User Input: The user inputs a number, and the program computes the sum.
+
 #include <iostream>
 using namespace std;
 
