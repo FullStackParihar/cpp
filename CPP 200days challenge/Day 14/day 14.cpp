e587e0188d6dc5d5307b67989ecb28b0ad20381a
vishnuparihar <vishnuparihar239925@gmail.com>
2024-09-25 22:12:16 +0530

    assignment

---
+++ CPP 200days challenge/Day 14/day 14.cpp
@@ -0,0 +1,21 @@
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 1, b;
+    cout << a << " ";
+    int count = 1;
+    
+    for (int i = 1; i < 10; i++) {
+        if (i % 2 != 0) {
+            a = pow(2, count);
+            count++;
+            cout << a << " ";
+        } else {
+            cout << a << " ";
+        }
+    }
+    return 0;
+}
+ 
